33de637a2bf9bb6794f7e858eb205fd6debca665
yakopeca <132225987+yakopeca@users.noreply.github.com>
2023-09-24 10:37:39 -0500

    Countries_List
    Adding countries I would like to visit

---
+++ data.js
@@ -0,0 +1,104 @@
+// Country data
+let countries = [
+    {
+      name: "Italy",
+      location: [41.872, 12.567],
+      population: 56000000
+    },
+    {
+      name: "Brazil",
+      location: [-14.2350, -51.925],
+      population: 214000000
+    },
+    {
+      name: "Switzerland",
+      location: [46.818, -8.227],
+      population: 8703000
+    },
+    {
+      name: "Ireland",
+      location: [53.412, -8.243],
+      population: 5033000
+    },
+    {
+      name: "Canary Islands",
+      location: [28.291, -16.6291],
+      population: 2207000
+    },
+    {
+      name: "Greece",
+      location: [39.074, 21.824],
+      population: 10640000
+    },
+    {
+      name: "Thailand",
+      location: [15.870, 100.992],
+      population: 71600000
+    },
+    {
+      name: "Australia",
+      location: [-25.274, 133.775],
+      population: 25690000
+    },
+    {
+      name: "Singapore",
+      location: [1.352, 103.819],
+      population: 5454000
+    },
+    {
+      name: "Iceland",
+      location: [64.963, -19.020],
+      population: 372520
+    },
+    {
+      name: "Netherlands",
+      location: [52.132, 5.291],
+      population: 17530000
+    },
+    {
+      name: "New Zealand",
+      location: [-42.866, 172.729],
+      population: 5123000
+    },
+    {
+      name: "Australia",
+      location: [-25.274, 133.775],
+      population: 25690000
+    },
+    {
+      name: "Ireland",
+      location: [52.912, -9.225],
+      population: 5033000
+    },
+    {
+      name: "Cambodia",
+      location: [13.014, 104.229],
+      population: 16590000
+    },
+    {
+      name: "Austria",
+      location: [47.516231, 14.550072],
+      population: 8956000
+    },
+    {
+      name: "Hong Kong",
+      location: [22.396, 114.109],
+      population: 7413000
+    },
+    {
+      name: "Germany",
+      location: [51.165, 10.451],
+      population: 83000000
+    },
+    {
+      name: "Ecuador",
+      location: [-0.847, -78.846],
+      population: 17800000
+    },
+    {
+      name: "Canada",
+      location: [52.287, -88.690],
+      population: 38250000
+    }
+  ];
+  
